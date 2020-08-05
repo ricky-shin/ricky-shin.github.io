b77053806a78ab88f8a3c4c271dc7aace206e8ad
Ricky Shin <39282962+ricky-shin@users.noreply.github.com>
2020-08-04 21:52:13 -0400

    Updated home with new project + new 404 page

---
+++ astronaut.js
@@ -0,0 +1,9 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('astroAnimation'),
+        
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'imgs/404/astronaut.json'
+        
+    })
