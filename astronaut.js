93e5307b10ff3dbd3bccf9a4961a0fedca740432
Ricky Shin <richyongshin@gmail.com>
2020-10-25 21:11:55 -0400

    Revamped Personal Website
    - More minimal page
    - One page layout
    - Brief descriptions for each project

--- astronaut.js
+++
@@ -1,9 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('astroAnimation'),
-        
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: '/imgs/404/astronaut.json'
-        
-    })
